66ff14ca6f7d05a8eabf3ca8b59a0d81a95423c7
Natasha Hull <musicalnat@gmail.com>
2013-10-19 02:11:57 -0700

    Finished organizing and creating initial specs

--- spec/crud_generator_spec.rb
+++ spec/crud_generator_spec.rb
@@ -2,9 +2,16 @@ require 'rspec'
 require 'crud_generator'
 
 describe CRUDGenerator do
+  context "Controller variables" do
+    it "has the right instance name"
+    it "has the right model class"
+    it "has the right index_url"
+  end
 
-  it "creates all crud methods automatically"
-  it "add all and only specified crud methods (when specified)"
-  it "only creates included crud methods"
-  it "creates all crud methods except ones that are excluded"
+  context "Controller actions" do
+    it "creates all crud methods automatically"
+    it "add all and only specified crud methods (when specified)"
+    it "only creates included crud methods"
+    it "creates all crud methods except ones that are excluded"
+  end
 end
