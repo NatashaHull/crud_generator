2be843c1ea1016769523e132204346915dc50a45
Natasha Hull <musicalnat@gmail.com>
2013-10-19 01:53:22 -0700

    stubbed in some tests

--- spec/crud_generator_spec.rb
+++ spec/crud_generator_spec.rb
@@ -2,4 +2,9 @@ require 'rspec'
 require 'crud_generator'
 
 describe CRUDGenerator do
+
+  it "creates all crud methods automatically"
+  it "add all and only specified crud methods (when specified)"
+  it "only creates included crud methods"
+  it "creates all crud methods except ones that are excluded"
 end
