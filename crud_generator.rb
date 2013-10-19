e654b89fe05b26150f803868b24836181e226a26
Natasha Hull <musicalnat@gmail.com>
2013-10-19 01:37:09 -0700

    Finished initial code

---
+++ crud_generator.rb
@@ -0,0 +1,104 @@
+module CRUDGenerator
+  def generate_crud_actions(*args)
+    actions = args ||
+              [:index, :show, :new, :create, :edit, :update, :delete]
+
+    actions.each do |action|
+      send("define_#{action}")
+    end
+  end
+
+  def define_index
+    define_method(:index) do
+      instance_variable_set(
+        "@#{instance_name.pluralize}",
+        model_class.all
+        )
+    end
+  end
+
+  def define_show
+    define_method(:show) do
+      instance_variable_set(
+        "@#{instance_name}",
+        model_class.find(params[:id])
+        )
+    end
+  end
+
+  def define_new
+    define_method(:new) do
+      instance_variable_set(
+        "@#{instance_name}",
+        model_class.new
+        )
+    end
+  end
+
+  def define_edit
+    define_method(:edit) do
+      instance_variable_set(
+        "@#{instance_name}",
+        model_class.find(params[:id])
+        )
+    end
+  end
+
+  def define_create
+    define_method(:create) do
+      instance_variable_set(
+        "@#{instance_name}",
+        model_class.new(params[instance_name.to_sym])
+        )
+
+      obj = instance_variable_get("@#{instance_name}")
+
+      if obj.save
+        redirect_to obj
+      else
+        flash.now[:errors] = obj.errors.full_messages
+        render :new
+      end
+    end
+  end
+
+  def define_update
+    define_method(:update) do
+      instance_variable_set(
+        "@#{instance_name}",
+        model_class.find(params[:id])
+        )
+
+      obj = instance_variable_get("@#{instance_name}")
+
+      if obj.update_attributes(params[instance_name.to_sym])
+        redirect_to obj
+      else
+        flash.now[:errors] = obj.errors.full_messages
+        render :edit
+      end
+    end
+  end
+
+  def define_destroy
+    define_method(:destroy) do
+      obj = model_class.find(params[:id])
+
+      obj.destroy
+      redirect_to index_url
+    end
+  end
+
+  def instance_name
+    self.class.to_s.delete("Controller").singularize
+  end
+
+  def model_class
+    instance_name.camelize.constantize
+  end
+
+  def index_url
+    url = root_url
+    url += "/#{model_class.pluralize}"
+  end
+end
